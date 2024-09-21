c78c038c2b3f107f537ce451e99b37033ea3386f
Tony Thomas <tony@phaethon.tech>
2024-09-22 01:29:34 +0530

    Add pages and move ChartComponent to Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Center } from "@chakra-ui/react";
+
+import ChartComponent from "@/components/ChartComponent/ChartComponent";
+
+const Home = () => {
+  return (
+    <>
+      <Center>
+        <ChartComponent />
+      </Center>
+    </>
+  );
+};
+
+export default Home;
